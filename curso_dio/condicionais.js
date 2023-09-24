953c37e2a25eaf32a42176ccf85e28bb6f5e88b7
leandroleekrap <leandroleekrap@gmail.com>
2023-09-24 10:30:46 -0300

    Estrutura if else]

---
+++ curso_dio/condicionais.js
@@ -0,0 +1,14 @@
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+if (jogador1 > jogador2) {
+    placar = "Jogador 1 ganhou!";
+    console.log(placar);
+}else if(jogador1 == jogador2){
+    placar = "Empate";
+    console.log(placar);
+}else{
+    placar = "Jogador 2 ganhou!";
+    console.log(placar);
+}
