0c6ff04897dd35a3a16580e7314d2f6d050c4ceb
leandroleekrap <leandroleekrap@gmail.com>
2023-09-24 10:37:30 -0300

    Uso do prompt e alert javascript

--- curso_dio/condicionais.js
+++ curso_dio/condicionais.js
@@ -2,6 +2,12 @@ var jogador1 = 0;
 var jogador2 = 0;
 var placar;
 
+
+jogador1 = prompt("Jogador 1: Digite um número de 1 a 6");
+jogador2 = prompt("Jogador 2: Digite um número de 1 a 6");
+placar = parseInt(jogador1) + parseInt(jogador2);
+alert(`O resultado da soma dos números é ${placar}`);
+
 if (jogador1 > jogador2) {
     placar = "Jogador 1 ganhou!";
     console.log(placar);
