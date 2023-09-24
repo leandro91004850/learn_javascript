e1ddf1753c6703323e24eec1156a054957d2d047
leandroleekrap <leandroleekrap@gmail.com>
2023-09-24 11:10:45 -0300

    For/in => repetição a partir de uma propriedade

--- curso_dio/lacos_repeticoes.js
+++ curso_dio/lacos_repeticoes.js
@@ -1,6 +1,12 @@
 let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5', 'valor6',];
 let object = {propriedade: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3',}
 
-for(let i = 0; i <= array.length; i++ ){
-    console.log(i);
+/*for(let i = 0; i <= array.length; i++ ){
+    console.log(array);
+}
+*/
+
+
+for(let i in array){
+    console.log(array[i])
 }
