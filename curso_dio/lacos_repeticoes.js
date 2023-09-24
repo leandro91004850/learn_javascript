418ebd65607659aed26de2a3a340a4dffe0bf348
leandroleekrap <leandroleekrap@gmail.com>
2023-09-24 11:48:35 -0300

    Estrutura do while

--- curso_dio/lacos_repeticoes.js
+++ curso_dio/lacos_repeticoes.js
@@ -1,13 +1,16 @@
-let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5', 'valor6'];
+let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5', 'valor6', 'valor7'];
 let object = {
     propriedade: 'objeto 1', 
     propriedade2: 'objeto 2',
      propriedade3: 'objeto 3'
     }
 
+
 var i = 0;
 
-while(i <= array.length){
+do{
+
     console.log(array[i]);
     i++;
-}
+
+}while(i <= array.length);
