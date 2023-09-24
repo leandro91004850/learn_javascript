287c47a1dd292c7b35be3f5c1dd19ae7021fa72e
leandroleekrap <leandroleekrap@gmail.com>
2023-09-24 09:57:01 -0300

    Metodos splice e slice

--- curso_dio/variables_vectors.js
+++ curso_dio/variables_vectors.js
@@ -2,5 +2,13 @@
 
 let arrays = ['String', 1, true, ['array1'], ['array2'], ['array3'], ['array4']]
 
-arrays.pop();
-console.log(arrays)
+/*arrays.pop();
+console.log(arrays);*/
+
+/*arrays.splice(0, 3);
+console.log(arrays);*/
+
+/* 
+let novoArrays = arrays.slice(0, 3);
+console.log(novoArrays);
+*/
