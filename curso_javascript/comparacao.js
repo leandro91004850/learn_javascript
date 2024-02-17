fd9e282d0d9d6fcb1775680943093efe23e14f88
leandroleekrap <leandroleekrap@gmail.com>
2024-02-17 15:54:41 -0300

    Alguns operadores de comparaão

---
+++ curso_javascript/comparacao.js
@@ -0,0 +1,17 @@
+let tipo  = 1;
+
+if(tipo == "1") {
+    console.log("1 é igual a '1', em comparação com valor");
+}
+
+if(tipo === "1") {
+    console.log("1 é igual a '1', e o tipo é igual"); // correto a se usar no dia a dia
+}
+
+if(tipo != "1") {
+    console.log("o valor é diferente de 1");
+}
+
+if(tipo !== "1") {
+    console.log("o valor e o tipo são diferentes de 1");
+}
