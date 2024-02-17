e4c4a8f66f5a94270d0c9c676029265149061d56
leandroleekrap <leandroleekrap@gmail.com>
2024-02-17 20:56:28 -0300

    (use strict) usado para eliminar erros silenciosos do javascript

---
+++ curso_javascript/strict.js
@@ -0,0 +1,10 @@
+// 'use strict' => usado para evitar passar batido erros do antigo javascript
+
+'use strict';
+
+const test = function(){
+    resultado = 2 + 4;
+    return resultado;
+}
+
+console.log(test());
