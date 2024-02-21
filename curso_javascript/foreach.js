de960c7921b80e4f79c867aec371cb38a806ceab
leandroleekrap <leandroleekrap@gmail.com>
2024-02-21 15:37:31 -0300

    foreach

---
+++ curso_javascript/foreach.js
@@ -0,0 +1,18 @@
+const myArrayObj = [
+    {
+      nome: "Dener",
+      sobreNome: "Troquatte",
+    },
+    {
+      nome: "Nay",
+      sobreNome: "Troquatte",
+    },
+    {
+      nome: "José",
+      sobreNome: "Troquatte",
+    },
+  ];
+  
+  myArrayObj.forEach((item, index) => {  
+    console.log(index, item);
+  });
