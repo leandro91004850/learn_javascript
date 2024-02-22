5943bd6c334022a9a74106004d3255bda8ac393f
leandroleekrap <leandroleekrap@gmail.com>
2024-02-22 12:45:19 -0300

    Manipulando html textContent e inneText

---
+++ curso_javascript/DOM/textContext.js
@@ -0,0 +1,6 @@
+let p = document.querySelector("p");
+
+console.log(p.textContent); // com formatação
+console.log(p.innerText); // sem formatação
+
+p.textContent = "Novo texto"; // altera o texto do elemento
