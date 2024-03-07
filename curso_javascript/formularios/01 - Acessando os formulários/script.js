c896240e24f0404441ab300b41853c9aead40ef1
leandroleekrap <leandroleekrap@gmail.com>
2024-03-07 12:23:23 -0300

    formData validation

--- curso_javascript/formularios/01 - Acessando os formulários/script.js
+++ curso_javascript/formularios/01 - Acessando os formulários/script.js
@@ -3,9 +3,18 @@
 const form = document.forms.namedItem("registration");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // Evita que o formulário seja enviado
-  const name = form.name.value;
-  const password = form.password.value;
-  console.log(name, password);
+    event.preventDefault(); // Evita que o formulário seja enviado
+    const name = form.name.value;
+    const password = form.password.value;
+    //console.log(name, password);
 
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("password", password);
+
+    if(formData.get("name") === "admin" && formData.get("password") === "1234"){
+        console.log("Usuário autenticado");
+    } else {
+        console.log("Usuário não autenticado");
+    }
 });
